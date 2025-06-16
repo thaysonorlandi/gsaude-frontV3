06d90e59c55c1cfb6cae3f49671339da0fdc5eee
thaysonorlandi <thaysonorlandi16@gmail.com>
2025-06-16 16:59:52 -0300

    criado a página de login

--- gsaude-front/src/Pages/login/login.jsx
+++ gsaude-front/src/Pages/login/login.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import { useState } from 'react';
+import Logo from '../../assets/logoGSaude.png';
+import PersonIcon from '@mui/icons-material/Person';
+import LoginIcon from '@mui/icons-material/Login';
+import Button from '@mui/material/Button';
+import './login.css';
 
-export const login = () => {
+function Login() {
+  const [form, setForm] = useState({ usuario: '', senha: '' });
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert('Login realizado!');
+  }
+
   return (
-    <div>login</div>
-  )
+    <div className="background">
+      <div className="login-container">
+        <img src={Logo} alt="Logo GSaude" style={{ width: 200, marginBottom: 24 }} />
+        <form onSubmit={handleSubmit}>
+          <div className='input-icon'>
+            <PersonIcon />
+            <input
+              type="text"
+              name="usuario"
+              placeholder="Usuário"
+              value={form.usuario}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className='input-icon'>
+            <LoginIcon />
+            <input
+              type="password"
+              name="senha"
+              placeholder="Senha"
+              value={form.senha}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <Button color="primary" type="submit">Entrar</Button>
+          <Button type="button" className="btn-cadastro" onClick={() => alert('Cadastro ainda não implementado')}>
+            Cadastre-se
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+export default Login;
--- gsaude-front/src/routes/index.jsx
+++ gsaude-front/src/routes/index.jsx
@@ -1,17 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Home from '../pages/home';
-import Login from '../pages/login';
-
+import Home from '../pages/home/home';
+import Login from '../pages/login/login';
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Home />,
-    },
-    {
-        path: '/login',
-        element: <Login />,
-    },
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
 ]);
 
 export default router;
